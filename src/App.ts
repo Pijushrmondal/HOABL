4ec433defc44fe721cbf393c965ca6dad6de7f5b
Pijush Mondal <pijush@antino.com>
2025-09-15 11:35:50 +0530

    b-live_proxy_server_setup

--- src/App.ts
+++ src/App.ts
@@ -2,29 +2,16 @@ import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import morgan from "morgan";
 const app = express();
-const basePort = 8000;
+const PORT = 4000;
 
-const options8002 = {
-  target: `http://localhost:${basePort + 2}`,
+const optionsAdminBe = {
+  target: `http://localhost:${PORT + 4}`,
   changeOrigin: true,
-};
+  pathRewrite: {
+    "^/api": "/fleet/api",  // rewrite /api → /fleet/api
+  },
+}
 
-const options8003 = {
-  target: `http://localhost:${basePort + 3}`,
-  changeOrigin: true,
-};
-
-const options8008 = {
-  target: `http://localhost:${basePort + 8}`,
-  changeOrigin: true,
-};
-
-const options8005 = {
-  target: `http://localhost:${basePort + 5}`,
-  changeOrigin: true,
-};
-
-const PORT = 9000;
 
 // Morgan Middleware for logging
 app.use(morgan("dev"));
@@ -33,35 +20,10 @@ app.get("/", (req, res) => {
   res.json(`common gate way run at port ${PORT}`);
 });
 
-// Proxy paths based on some criteria, e.g., path starts with /api1 goes to port 3000
 app.use(
-  "/hoabl-admin",
-  (req, res, next) => {
-    // console.log(req);
-    next();
-  },
-  createProxyMiddleware(options8002),
+  "/fleet",
+  createProxyMiddleware(optionsAdminBe),
 );
-app.use(
-  "/hoabl-customer",
-  (req, res, next) => {
-    // console.log(req.headers);
-    next();
-  },
-  createProxyMiddleware(options8003),
-);
-
-app.use(
-  "/hoabl-customer-temp",
-  (req, res, next) => {
-    // console.log(req.headers);
-    next();
-  },
-  createProxyMiddleware(options8008),
-);
-app.use("/hoabl-payment", createProxyMiddleware(options8005));
-
-// Start the proxy server on port 5000
 
 app.listen(PORT, () => {
   console.log(`Proxy server is running on http://localhost:${PORT}`);
