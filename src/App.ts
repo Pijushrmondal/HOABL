589ddc4b2106c6b541dda3c134232b0d6c334211
Pijush Ray Mondal <pijush@antino.com>
2025-11-04 11:03:58 +0530

    added consumer

--- src/App.ts
+++ src/App.ts
@@ -8,12 +8,83 @@ const servicePort = 4000;
 const consumerTarget = `http://localhost:${servicePort + 1}`;
 const consumerService = `/consumer/api`;
 
+const adminTarget = `http://localhost:${servicePort + 1}`;
+const adminService = `/consumer/api`;
+
+const consumerOnboardTarget = `http://localhost:${servicePort + 1}`;
+const consumerOnboardService = `/consumer/api`;
+
+const fleetPayoutTarget = `http://localhost:${servicePort + 1}`;
+const fleetPayoutService = `/consumer/api`;
+
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Gateway running ✅");
 });
 
+app.use(
+  consumerService,
+  createProxyMiddleware({
+    target: consumerTarget,
+    changeOrigin: true,
+    pathRewrite: (path, req) => {
+      return `${consumerService}${path}`;
+    },
+  }),
+);
+
+app.use(
+  consumerService,
+  createProxyMiddleware({
+    target: consumerTarget,
+    changeOrigin: true,
+    pathRewrite: (path, req) => {
+      return `${consumerService}${path}`;
+    },
+  }),
+);
+
+app.use(
+  consumerService,
+  createProxyMiddleware({
+    target: consumerTarget,
+    changeOrigin: true,
+    pathRewrite: (path, req) => {
+      return `${consumerService}${path}`;
+    },
+  }),
+);
+app.use(
+  consumerService,
+  createProxyMiddleware({
+    target: consumerTarget,
+    changeOrigin: true,
+    pathRewrite: (path, req) => {
+      return `${consumerService}${path}`;
+    },
+  }),
+);
+app.use(
+  consumerService,
+  createProxyMiddleware({
+    target: consumerTarget,
+    changeOrigin: true,
+    pathRewrite: (path, req) => {
+      return `${consumerService}${path}`;
+    },
+  }),
+);
+app.use(
+  consumerService,
+  createProxyMiddleware({
+    target: consumerTarget,
+    changeOrigin: true,
+    pathRewrite: (path, req) => {
+      return `${consumerService}${path}`;
+    },
+  }),
+);
 app.use(
   consumerService,
   createProxyMiddleware({
